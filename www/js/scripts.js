2cf553b51c49010fd3561ebad11f1cdcd2cfe8fc
ctm2142 <ctm2142@rit.edu>
2017-05-15 04:03:36 -0400

    Basic p5 start - Remove overflow - Draw all pianos initially   - Math for drawing pianos
    Need: draw keys directly instead of redrawing all each time (takes too long)

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -25,6 +25,7 @@ var channelColors = [
     new THREE.Color('aqua')
 ]
 var activeNotesThree = [];
+var activeNotesP5 = [];
 
 // Set up defaults
 function init() {
@@ -35,6 +36,7 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     for(var i = 0; i < 16; i++) {
         activeNotesThree[i] = [];
+        activeNotesP5[i] = [];
     }
 }
 
@@ -123,10 +125,10 @@ function clearScene() {
     }
 }
 
+init();
+
 // Start JS things when page ready
 $(document).ready( function() {
-    init();
-
     document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
     MIDI.loadPlugin({
@@ -172,10 +174,15 @@ $(document).ready( function() {
             var ptObj = new THREE.Points( ptsGeometry, ptsMaterial );
             sceneThree.add( ptObj );
             activeNotesThree[channel][note] = ptObj;
+            activeNotesP5[channel].push(note);
+            redraw()
         } else if(message == 128) { // noteOff
             if(activeNotesThree[channel][note] == null)
                 return;
             sceneThree.remove(activeNotesThree[channel][note]);
+            delete activeNotesThree[channel][note];
+            delete activeNotesP5[channel][activeNotesP5[channel].indexOf(note)];
+            redraw();
         } else if(message == 123) {
             clearScene();
         }
@@ -197,15 +204,64 @@ function render() {
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('pianoroll');
+    noLoop();
+
+    var yMin = $("header").height();
+    var xI = windowWidth;
+    var yI = (windowHeight - yMin) / 16;
+    var paddingX = 10;
+    var paddingY = 5;
+    for(var i = 0; i < 16; i++) {
+    //for(var i = 0; i < 1; i++) {
+        drawPiano(paddingX, yMin + (yI * i) + paddingY, xI - paddingX, yMin + (yI * (i+1)) - paddingY, i);
+    }
 }
 function draw() {
-    
+    //console.log('draw');
 }
-function drawPiano(x1, y1, x2, y2) {
-    var padding = 10;
-
+function drawKeyDirect(channel, note) {
+    // TODO
 }
-function drawOctave(x1, y1, x2, y2) {
-
+function drawPiano(x1, y1, x2, y2, channel) {
+    //console.log('draw piano: (' + x1 + ',' + y1 + ') to (' + x2 + ',' + y2 + ')');
+    var xI = (x2 - x1) / 75;
+    for(var i = 0; i < 11; i++) {
+        if (i == 10) {
+            drawOctave(x1 + (xI * 7 * i), y1, x1 + (xI * 7 * i) + (xI * 5), y2, i, channel);
+        } else {
+            drawOctave(x1 + (xI * 7 * i), y1, x1 + (xI * 7 * i) + (xI * 7), y2, i, channel);
+        }
+    }
 }
-function drawKey(x1, x2, y1, y2, )
+function drawOctave(x1, y1, x2, y2, octave, channel) {
+    //console.log('draw octave: (' + x1 + ',' + y1 + ') to (' + x2 + ',' + y2 + ')');
+    var wholeNoteNums = [0, 2, 4, 5, 7, 9, 11];
+    var halfNoteNums = [1, 3, 6, 8, 10];
+    var yI = (y2 - y1) / 2;
+    if(octave == 10) {
+        var xI = (x2 - x1) / 5;
+        var totalWhole = 5;
+        var totalHalf = 3;
+    } else {
+        var xI = (x2 - x1) / 7;
+        var totalWhole = 7;
+        var totalHalf = 5;
+    }
+    for(var i = 0; i < totalWhole; i++) {
+        drawKey(x1 + (xI * i), y1 + yI, x1 + (xI * (i + 1)), y2, (12 * octave) + wholeNoteNums[i], false, channel);
+    }
+    for(var i = 0; i < totalHalf; i++) {
+        drawKey(x1 + (xI * i) + (xI / 2) + (i > 1 ? xI : 0), y1, x1 + (xI * (i + 1)) + (xI / 2) + (i > 1 ? xI : 0), y1 + yI, (12 * octave) + halfNoteNums[i], true, channel);
+    }
+}
+function drawKey(x1, y1, x2, y2, note, half, channel) {
+    //console.log('draw key: (' + x1 + ',' + y1 + ') to (' + x2 + ',' + y2 + ')');
+    if(half) {
+        fill(color('dimgrey'));
+    } else {
+        fill(color('darkgrey'));
+    }
+    stroke(color('black'));
+    strokeWeight(2);
+    rect(x1, y1, x2 - x1, y2 - y1);
+}
